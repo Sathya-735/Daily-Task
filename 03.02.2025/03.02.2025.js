9853405bed1b6c88a87e3ceea2ebe261dad97bbf
Sathya-735 <sathyasiva735@gamil.com>
2025-02-04 10:59:16 +0530

    8 completed pending 3

--- 03.02.2025/03.02.2025.js
+++ 03.02.2025/03.02.2025.js
@@ -1,6 +1,6 @@
 // 1. Given the fruits array, how would you find the position of "Banana"?
 
-let fruits=["Apple","Mango","Banana","Orange"];
+let fruits=["Apple","Mango","Banana","Orange","kiwi"];
 let find_the_position=fruits.indexOf("Banana");
 console.log("Task 1:",find_the_position);
 
@@ -12,7 +12,7 @@ console.log("Task 2:",Present_or_not);
 
 // 3. How would you create a shallow copy of the fruits array? 
 let num=[1,2,3,4];
-shallow_array=num;
+let shallow_array=num.slice();
 console.log("Task 3:",num);
 console.log("Task 3 Shallow copy:",shallow_array);
 
@@ -26,29 +26,45 @@ console.log("Task 4:After concat",fruits.concat(con_cat_array));
 
 // 5. How would you extract the first two elements from the fruits array?
 
-sli=[10,20,30];
+sli=[10,20,30,40];
 
 console.log("Task 5: Before slice",sli);
 
 console.log("Task 5: After slice",sli.slice(0,2));
 
 // 6. How can you filter out fruits that do not contain the letter "a" from the fruits array?
+// let fru=["Apple","Mango","Banana","Orange","kiwi"];
 
-
+// let do_not_contain_a=fru.find((v)=>{
+//     return v.length!=="A";
+// })
+// console.log("Task 6:",do_not_contain_a);
 
 // 7. How would you iterate over the fruits array and print each fruit using forEach()? 
 
-fruits.forEach((val)=>{
-    console.log("Task 7:",val);   
-})
+// let iterate_forEach=fru.forEach((vls)=>{
+//     return vls;
+// })
+// console.log(iterate_forEach);
 
+
+
+
+
+
+// 8. How can you check if every fruit name in the fruits array has more than 3 characters? 
+let frits=["Apple","Mango","Banana","Orange","kiwi","haw"];
+
+let morethan3=frits.map((frtlength)=>{
+    return frtlength.length>3;
+});
+console.log("Task 8:",morethan3);
 
 // 9. How would you find the first fruit in the fruits array that starts with the letter "O"? 
-let filter_O=fruits.filter((vl)=>{
-    // return vl.includes("O");
-    return vl.includes('O')
-})
-console.log("Task 9:",filter_O);
+
+
+
+
 
 
 // 10. How can you join all the elements of the fruits array into a single string separated by commas? 
@@ -59,11 +75,14 @@ console.log("Task 10:",join_by_commas);
 
 // 11. How would you remove the second element ("Banana") from the fruits array?
 
-let frt=["Apple","Banana","Mango"];
+let f=["Apple","Mango","Banana","Orange","kiwi","haw"];
+console.log("Task 11:",f)
+f.splice(2,1)
+console.log("Task 11:",f);
 
-let removed_array=frt.splice(1,1);
 
-console.log("Task 11:",removed_array);
+
+
 
 
 
